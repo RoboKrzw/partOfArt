5265509a82153a1f7fdebcf2d15e142243048c1e
RoboKrzw <r.krzywkowski@gmail.com>
2020-12-04 14:24:55 +0100

    poprawiono paralaxy

--- js/paralax.js
+++ js/paralax.js
@@ -1,20 +1,29 @@
 let left = document.getElementById('leftlion');
 let right = document.getElementById('rightlion');
 let span = document.getElementById('spanlion');
+let about = document.getElementById('about');
+
+let explore = $('#banner').css("height");
+$(function(){
+    
+    $( window ).on("resize", function() {
+       explore = $('#banner').css("height");
+    });
+});
+console.log(parseInt(explore));
+
 
 window.addEventListener('scroll', function(){
     let scroll = window.pageYOffset;
     
+console.log(parseInt(scroll));
     let speed = left.dataset.speed;
-    let scale = span.dataset.scale;
-    let speed1 = span.dataset.speed1;
-    left.style.transform = `translateY(${scroll * speed}px)`;
-    right.style.transform = `translateY(${scroll * speed}px)`;
+    // let scale = span.dataset.scale;
+    // let speed1 = span.dataset.speed1;
+    left.style.transform = `translateY(${(parseInt(explore) - scroll) * speed}px)`;
+    right.style.transform = `translateY(${(parseInt(explore) - scroll) * speed}px)`;
     // span.style.transform = `scale(${scroll * scale +1}, ${scroll * scale +1})`;
     // span.style.transform += `translateY(${scroll * speed1}px)`;
-
-
-    console.log(scroll);
 
     // let value = window.scrollY;
 
